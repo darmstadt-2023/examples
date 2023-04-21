21371596b5156f18e1df8979b519533e4ad6b0c5
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-04-21 20:07:25 +0300

    example 3 ready

---
+++ Console_example2/IPerson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console_example2
+{
+    public interface IPerson
+    {
+    }
+}
---
+++ Console_example2/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_example2
+{
+    internal abstract class Person
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+    }
+}
