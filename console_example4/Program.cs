9122020d7956203b65ff9f13ff8b86d7dacc921d
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-04-28 18:26:55 +0300

    exercise4 not ready

---
+++ console_example4/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+using System.Text;
+
+Console.WriteLine("Hello, Write the url!");
+string url=Console.ReadLine();
+	WebRequest wb = WebRequest.Create(url);
+	WebResponse rp = wb.GetResponse();
+	StreamReader reader = new StreamReader(rp.GetResponseStream(), Encoding.ASCII);
+	Console.WriteLine(reader.ReadToEnd());
